6ea3b00adbaff1739395857e59a46435aab8ab00
Ayush More <ayush08am@gmail.com>
2025-11-28 16:36:33 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -7,12 +7,12 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtiVFjnUXTxsfcIbuzz7EwxYkAJ-z0-2I",
-  authDomain: "fooddev1-b4c28.firebaseapp.com",
-  projectId: "fooddev1-b4c28",
-  storageBucket: "fooddev1-b4c28.appspot.com",
-  messagingSenderId: "931767479331",
-  appId: "1:931767479331:web:5791c46c7ef8a5f9569596"
+  apiKey: "",
+  authDomain: "",//your firebase code
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
